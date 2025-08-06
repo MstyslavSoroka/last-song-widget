1052c115bce7e722a86ee563de253b2b82d64d48
Mstyslav Soroka <mstyslav2010@gmail.com>
2025-08-06 13:40:31 +0100

    made widget responsive

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
         );
         const data = await response.json();
         setTrack(data.recenttracks.track[0]);
+        console.log(data);
       } catch (error) {
         console.error(error);
       }
@@ -24,16 +25,18 @@ function App() {
 
   return (
     <div className="flex h-[100vh] justify-center items-center">
-      <div className="rounded-2xl border-1 border-white flex p-20 pt-5 pb-5 ">
+      <div className="rounded-2xl border-1 border-white flex flex-col md:flex-row p-5 md:pr-20 md:pl-20 pt-5 pb-5 gap-5">
         {track ? (
           <>
             <img
-              className="rounded-full w-50 mr-10 animate-spin-slow "
+              className="rounded-full w-50 animate-spin-slow m-0 m-auto "
               src={track.image[3]['#text']}
               alt="album cover"
             />
-            <div className="flex flex-col justify-center gap-3 text-white">
-              <h3 className="title font-black text-3xl">{track.name}</h3>
+            <div className="text-center flex flex-col justify-center gap-3 text-white md:text-left">
+              <h3 className="text-xl title font-black md:text-3xl">
+                {track.name}
+              </h3>
               <p className="author">{track.artist['#text']}</p>
               {(track['@attr']?.nowplaying === 'true' && (
                 <p>
@@ -47,6 +50,7 @@ function App() {
           <p className="text-white">Loading...</p>
         )}
       </div>
+      ``
     </div>
   );
 }
